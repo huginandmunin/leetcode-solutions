e19e2294480cc04ce9ad7aba9d6ddab2e59bf9cc
George Thomas <gcthomas@uw.edu>
2021-10-21 09:48:08 -0700

    Problem 136 single number

---
+++ python/p136_single_number.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """ Counts for each number are stored in a dictionary """
+        counts_dict = {}
+
+        for n in nums:
+            if n in counts_dict.keys():
+                counts_dict[n] += 1
+            else:
+                counts_dict[n] = 1
+        for k, v in counts_dict.items():
+            if v == 1:
+                return k
+        
---
+++ python/tests/test_p136.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append("..")
+
+from p136_single_number import Solution
+
+def test_solution1():
+    solution = Solution()
+    input = [2,2,1]
+    expected = 1
+    assert solution.singleNumber(input)==expected
+
+def test_solution2():
+    solution = Solution()
+    input = [4,1,2,1,2]
+    expected = 4
+    assert solution.singleNumber(input)==expected
+
+def test_solution3():
+    solution = Solution()
+    input = [1]
+    expected = 1
+    assert solution.singleNumber(input)==expected

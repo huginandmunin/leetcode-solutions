13568e0883ea1dbb4324c6c23629fe1bc02df5c0
George Thomas <gcthomas@uw.edu>
2021-11-04 22:10:52 -0700

    Problem 145 Binary Tree Postorder Traversal

---
+++ python/tests/test_p145.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append("..")
+
+from p145_binary_tree_postorder_traversal import Solution, TreeNode
+
+def test_solution1():
+    # input = [1]
+    solution = Solution()
+    input_tree = TreeNode(1)
+    expected = [1]
+    assert solution.postorderTraversal(input_tree)==expected
+
+def test_solution2():
+    # input = []
+    solution = Solution()
+    input_tree = None
+    expected = []
+    assert solution.postorderTraversal(input_tree)==expected
+
+def test_solution3():
+    # input = [1,2]
+    #   1
+    #  /
+    # 2
+    solution = Solution()
+    input_tree = TreeNode(1)
+    input_tree.left = TreeNode(2)
+    expected = [2,1]
+    assert solution.postorderTraversal(input_tree)==expected
+
+def test_solution4():
+    # input = [1,None,2]
+    #  1
+    #   \
+    #    2
+    solution = Solution()
+    input_tree = TreeNode(1)
+    input_tree.right = TreeNode(2)
+    expected = [2,1]
+    assert solution.postorderTraversal(input_tree)==expected
+
+def test_solution5():
+    # input = [1,None,2,3]
+    #  1
+    #   \
+    #    2
+    #   /
+    #  3
+    solution = Solution()
+    input_tree = TreeNode(1)
+    input_tree.right = TreeNode(2)
+    input_tree.right.left = TreeNode(3)
+    expected = [3,2,1]
+    assert solution.postorderTraversal(input_tree)==expected

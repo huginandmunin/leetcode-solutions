2a0850b3e321703b140da588579b0f719627e253
George Thomas <gcthomas@uw.edu>
2021-11-30 21:50:39 -0800

    P1431 Kids With the Greatest Number of Candies

---
+++ python/tests/test_p1431.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("..")
+
+from p1431_kids_with_the_greatest_number_of_candies import Solution
+
+
+def test_solution1():
+    solution = Solution()
+    candies = [2,3,5,1,3]
+    n = 3
+    expected = [True, True, True, False, True]
+    assert solution.kidsWithCandies(candies,n)==expected
+
+
+def test_solution2():
+    solution = Solution()
+    candies = [4,2,1,1,2]
+    n = 1
+    expected = [True, False, False, False, False]
+    assert solution.kidsWithCandies(candies,n)==expected
+
+
+def test_solution1():
+    solution = Solution()
+    candies = [12,1,12]
+    n = 10
+    expected = [True, False, True]
+    assert solution.kidsWithCandies(candies,n)==expected

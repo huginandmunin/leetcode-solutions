c1e5820347b001f871d89847d9f5cad8aff56b23
George Thomas <gcthomas@uw.edu>
2021-12-01 22:52:21 -0800

    Remove debugging code

--- python/p1103_distribute_candies_to_people.py
+++ python/p1103_distribute_candies_to_people.py
@@ -16,19 +16,5 @@ class Solution:
                 i = 0
             else:
                 i+= 1
-            print(f'end i = {i} c={candies}, g={give_num}, r={result}')
 
         return result
-
-
-if __name__ == "__main__":
-    solution = Solution()
-    candies = 7
-    num_people = 4
-    results = solution.distributeCandies(candies,num_people)
-    print(f' results={results}')
-
-    candies = 10
-    num_people = 3
-    results = solution.distributeCandies(candies,num_people)
-    print(f' results={results}')

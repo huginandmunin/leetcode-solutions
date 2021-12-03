1ff951f4be576fdf3f72f8e17a582125bdfa24b9
George Thomas <gcthomas@uw.edu>
2021-12-02 21:57:18 -0800

    Update comments

--- python/p1103_distribute_candies_to_people.py
+++ python/p1103_distribute_candies_to_people.py
@@ -2,6 +2,12 @@ from typing import List
 
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        """
+        This solution steps through the results array.
+
+        The hints mention adding the whole array until there aren't enough candies.
+        You could use the zip function to add [1,2,3,...] to your initial array [0,0,0,...]
+        """
 
         result = [0]*num_people
         give_num = 0

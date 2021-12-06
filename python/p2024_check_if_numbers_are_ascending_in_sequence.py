49bec4783d6a9d6b1c0bdc9667a568bacc17d9aa
George Thomas <gcthomas@uw.edu>
2021-12-05 23:34:06 -0800

    P2024 Check if Numbers are Ascending in Sequence

---
+++ python/p2024_check_if_numbers_are_ascending_in_sequence.py
@@ -0,0 +1,23 @@
+import re
+
+class Solution:
+    def areNumbersAscending(self, s: str) -> bool:
+        """
+        A sentence has words or numbers separated by spaces. Check that the numbers are increasing. 
+        eg, "a puppy has 2 eyes 4 legs".
+        
+        This solution will correctly handle an input string with 0 as the first number,
+        eg, "0 is more lonely than 1". 
+        Some solutions initialize the previous value as 0. Those will give an incorrect False
+        for the above input. 
+        """
+        sl = s.split()
+        previous = None
+        for t in sl:
+            if re.match('^\d+$',t):
+                t_int = int(t)
+                if previous and t_int <= previous:
+                    return False
+                previous = t_int
+
+        return True
---
+++ python/tests/test_p2024.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("..")
+
+from p2024_check_if_numbers_are_ascending_in_sequence import Solution
+
+
+def test_solution1():
+    solution = Solution()
+    s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles"
+    assert solution.areNumbersAscending(s)==True
+
+def test_solution2():
+    solution = Solution()
+    s = "hello world 5 x 5"
+    assert solution.areNumbersAscending(s)==False
+
+def test_solution3():
+    solution = Solution()
+    s = "sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s"
+    assert solution.areNumbersAscending(s)==False
+
+def test_solution4():
+    solution = Solution()
+    s = "4 5 11 26"
+    assert solution.areNumbersAscending(s)==True
+
+def test_solution5():
+    solution = Solution()
+    s = "0 is more lonely that 1"
+    assert solution.areNumbersAscending(s)==True

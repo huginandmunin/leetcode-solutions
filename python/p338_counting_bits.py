a1a79aefdaddb1a13ec598600aac4c43801ae9bb
George Thomas <gcthomas@uw.edu>
2022-10-24 21:31:51 -0700

    Update

--- python/p338_counting_bits.py
+++ python/p338_counting_bits.py
@@ -5,7 +5,3 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         # Get number of 1's per number and add to list
         return [Counter(bin(i))['1'] for i in range(n+1)]
-
-
-
-

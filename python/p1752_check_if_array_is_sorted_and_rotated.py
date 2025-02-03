d42b2a47b0092451b32d280b851617bbd5bbdbc3
George Thomas <gcthomas@uw.edu>
2025-02-02 17:25:20 -0800

    P1752 use alternate method

--- python/p1752_check_if_array_is_sorted_and_rotated.py
+++ python/p1752_check_if_array_is_sorted_and_rotated.py
@@ -6,6 +6,22 @@ class Solution:
         Brute force and check if it is possible for a sorted array to start from each position.
         """
 
+        # This method only needs one pass through the array. 
+        have_reset = False
+        prev = nums[0]
+        for num in nums:
+            if num < prev:
+                if have_reset:
+                    return False
+                have_reset = True
+            prev = num
+
+        if have_reset:
+            if nums[len(nums)-1] > nums[0]:
+                return False
+        return True
+            
+        # This was my first try, inspired by the hint to use "brute force"
         array_len = len(nums)
         for i in range(array_len):
             valid=True
